02b5bb39dcd85dfeda81f6816621cd8044e87053
mrharu_o <33688387+mrharu-o@users.noreply.github.com>
2022-12-19 21:05:26 +0900

    date

--- assets/js/simple_clock.js
+++ assets/js/simple_clock.js
@@ -1,9 +1,19 @@
-function Time() {
+function Date_get() {
+    var dates = new Date();
+    var years = dates.getFullYear();
+    var months = dates.getMonth();
+    var days = dates.getDate();
+    var today = years + "年" + months + "月" + days + "日";
+    document.getElementById("Date_place").innerHTML = today;
+    }
+    setInterval('Time()',1000);
+
+function Time_get() {
     var time = new Date();
     var hours = time.getHours();
     var minutes = time.getMinutes();
     var seconds = time.getSeconds();
     var clock = hours + ":" + minutes + ":" + seconds;
-    document.getElementById("Time-view").innerHTML = clock;
+    document.getElementById("Time_place").innerHTML = clock;
     }
     setInterval('Time()',1000);
